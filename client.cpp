901938c82ab1b37f5d1532ba27554e327d48c59f
Akhil Simha <akhilsimha1059@gmail.com>
2024-05-05 12:25:10 +0200

    Added Error Handling

--- client.cpp
+++ client.cpp
@@ -5,35 +5,59 @@
 
 using namespace std;
 
-int main() {
-  // Initialize Winsock
-  WSADATA wsaData;
-  WSAStartup(MAKEWORD(2, 2), &wsaData);
+int main()
+{
+    // Initialize Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        cerr << "WSAStartup failed with error: " << WSAGetLastError() << endl;
+        return 1;
+    }
 
-  // Create socket
-  int clientSocket = WSASocketA(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
+    // Create socket
+    int clientSocket = WSASocketA(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
+    if (clientSocket == INVALID_SOCKET)
+    {
+        cerr << "WSASocketA failed with error: " << WSAGetLastError() << endl;
+        WSACleanup();
+        return 1;
+    }
 
-  // Specify server address
-  SOCKADDR_IN serverAddress;
-  serverAddress.sin_family = AF_INET;
-  serverAddress.sin_port = htons(8080);
-  serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    // Specify server address
+    SOCKADDR_IN serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-  // Connect to server
-  connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    // Connect to server
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
+    {
+        cerr << "connect failed with error: " << WSAGetLastError() << endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
 
-  // Send message
-  const char* message = "Hello, server!";
-  send(clientSocket, message, strlen(message), 0);
+    // Send message
+    const char *message = "Hello, server!";
+    int bytesSent = send(clientSocket, message, strlen(message), 0);
+    if (bytesSent == SOCKET_ERROR)
+    {
+        cerr << "send failed with error: " << WSAGetLastError() << endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
 
-  // Print confirmation
-  cout << "Message sent to server: " << message << endl;
+    // Print confirmation
+    cout << "Message sent to server: " << message << endl;
 
-  // Close socket
-  closesocket(clientSocket);
+    // Close socket
+    closesocket(clientSocket);
 
-  // Cleanup Winsock
-  WSACleanup();
+    // Cleanup Winsock
+    WSACleanup();
 
-  return 0;
+    return 0;
 }
